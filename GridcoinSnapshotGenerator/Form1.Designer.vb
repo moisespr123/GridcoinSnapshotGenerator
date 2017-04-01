8bdfa6a58557417b84941e8e8958660e8ca0d4f1
Moises Cardona <moisesmcardona@gmail.com>
2017-04-01 09:25:08 -0400

    Add project files.

---
+++ GridcoinSnapshotGenerator/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 103)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(257, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Create Snapshot"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(176, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(197, 25)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(192, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Browse for a location to save Snapshot"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 54)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Create Snapshot every:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(12, 70)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(76, 20)
+        Me.NumericUpDown1.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(94, 77)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Hour(s)"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 138)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(204, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "0 hours create the snapshot one time only"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 175)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Gridcoin Snapshot Generator"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ GridcoinSnapshotGenerator/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If NumericUpDown1.Value = 0 Then
+            CreateSnapshot() 'Runs the snapshot one time only
+        Else
+            CreateSnapshot()
+            Timer1.Interval = NumericUpDown1.Value * 60 * 60 * 1000 'Hours * 60 minutes * 60 seconds * 1000 milliseconds
+            Timer1.Start()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveFileDialog1.Title = "Browse for a location to save Snapshot"
+        SaveFileDialog1.Filter = "7zip File (*.7z)|*.7z"
+        SaveFileDialog1.FileName = TextBox1.Text
+        SaveFileDialog1.ShowDialog()
+        If String.IsNullOrEmpty(SaveFileDialog1.FileName) = False Then TextBox1.Text = SaveFileDialog1.FileName
+    End Sub
+    Private Sub CreateSnapshot()
+        'We will send a signal to close the Gridcoin Wallet
+        Dim GridcoinProcess As Process() = Process.GetProcessesByName("gridcoinresearch")
+        For Each GRCProcess In GridcoinProcess
+            GRCProcess.CloseMainWindow()
+            'While loop to wait for the process to exit
+            While GRCProcess.HasExited = False
+                'Waits 1 second, then it will check again if the process has exited
+                Threading.Thread.Sleep(1000)
+            End While
+            'As of this moment, the Gridcoin Wallet should has been closed properly
+        Next
+        'Now, we will compress the txleveldb folder and the blk0001.dat file
+        Dim CompressionProcess As New ProcessStartInfo("C:\Program Files\7-zip\7z.exe")
+        Dim AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\GridcoinResearch"
+        CompressionProcess.Arguments = "a -m0=LZMA2 -mmt -mx9 """ & Path.GetDirectoryName(TextBox1.Text) & "\" & Path.GetFileNameWithoutExtension(TextBox1.Text) & " " & DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss tt") & ".7z"" """ & AppDataPath & "\txleveldb"" """ & AppDataPath & "\blk0001.dat"""
+        Dim StartProcess As Process = Process.Start(CompressionProcess)
+        StartProcess.WaitForExit()
+        'Backup should now be complete!
+        If My.Computer.FileSystem.FileExists("C:\Program Files (x86)\GridcoinResearch\gridcoinresearch.exe") Then
+            Process.Start("C:\Program Files (x86)\GridcoinResearch\gridcoinresearch.exe")
+        ElseIf My.Computer.FileSystem.FileExists("C:\Program Files\GridcoinResearch\gridcoinresearch.exe") Then
+            Process.Start("C:\Program Files\GridcoinResearch\gridcoinresearch.exe")
+        Else
+            MsgBox("GridcoinResearch.exe could not be found. Please launch the wallet manually")
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        CreateSnapshot()
+    End Sub
+End Class
